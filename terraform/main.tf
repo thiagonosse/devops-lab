28f2f9fc28f8410d31587c7bd6caab5993a917af
Thiago <thiagotabata2012@gmail.com>
2026-08-20 19:45:14 -0300

    ci: use remote terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "devops-lab-tfstate-155638725921"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
